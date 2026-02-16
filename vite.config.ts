f06b19dbda1530041ffebda166f6f3a0225af437
Theo Bayenet <dev.js.theo@gmail.com>
2026-02-16 02:41:24 +0100

    fix: force classic jsx runtime

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
