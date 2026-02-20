ee7a7054c7d01b895be37f3534f05672f8e8b0a8
Theo Bayenet <dev.js.theo@gmail.com>
2026-02-20 16:25:09 +0100

    fix(playground): setup asset prefix in production

--- playground/next.config.ts
+++ playground/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: `/${process.env.NEXT_PUBLIC_REPO_NAME ?? ""}`,
 };
 
 export default nextConfig;
